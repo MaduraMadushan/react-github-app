7926b01173437e94d52dbd76d67014f6290deab6
Madura Eeswara <madura.madushan@outlook.com>
2019-08-05 12:50:43 +0530

    clear cash

--- src/components/pages/Developer/Profile/Profile.js
+++ src/components/pages/Developer/Profile/Profile.js
@@ -1,13 +1,27 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { Link } from 'react-router-dom'
 import User from './User'
 import Overview from './Overview'
+import GithubContext from './../../../../context/github/githubContext'
 
 const Profile = ({ match }) => {
+  const githubContext = useContext(GithubContext)
+  const {
+    clearUser,
+    clearFollowers,
+    clearFollowing,
+    clearRepos
+  } = githubContext
+  const clear = () => {
+    clearUser()
+    clearFollowers()
+    clearFollowing()
+    clearRepos()
+  }
   return (
     <section className='mt-5'>
       <div className='container'>
-        <Link to='/developer' className='btn btn-dark my-3'>
+        <Link to='/developer' onClick={clear} className='btn btn-dark my-3'>
           Back To Search
         </Link>
         <div className='row'>
