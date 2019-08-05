2f4d96c99403af0eeed3add107652ebef669254e
Madura Eeswara <madura.madushan@outlook.com>
2019-08-05 12:32:55 +0530

    Search

--- src/components/pages/Developer/Search.js
+++ src/components/pages/Developer/Search.js
@@ -1,19 +1,31 @@
-import React from 'react'
+import React, { useState, useContext } from 'react'
+import GithubContext from './../../../context/github/githubContext'
 
 const Search = () => {
+  const githubContext = useContext(GithubContext)
+  const [text, setText] = useState('')
+
+  const onChange = e => setText(e.target.value)
+
+  const onSubmit = e => {
+    e.preventDefault()
+    githubContext.searchUsers(text)
+    setText('')
+  }
   return (
-    <form className='py-5'>
+    <form onSubmit={onSubmit} className='py-5'>
       <input
         type='text'
         className='form-control'
         placeholder='Search Users...'
+        value={text}
+        onChange={onChange}
       />
       <input
         className='btn btn-dark btn-block mt-3'
         type='submit'
         value='Search'
       />
-      <button className='btn btn-light btn-block mt-3'>Clear</button>
     </form>
   )
 }
